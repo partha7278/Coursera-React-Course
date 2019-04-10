297cf4c324201407c918950ccea6f18bafa1df28
Partha <partha4224@gmail.com>
2019-04-10 10:12:55 +0530

    Week-3 Controlled Form

--- confusion/src/components/AboutComponent.js
+++ confusion/src/components/AboutComponent.js
@@ -24,7 +24,7 @@ function About(props) {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <RenderLeader leader={leader} />
+            <RenderLeader leader={leader} key={leader.id} />
         );
     });
 
